7d189ae3d32e923e3c3f5656f262d065d333dcde
nurabdi2776 <nurabdi2776@gmail.com>
2026-07-12 07:36:37 -0700

    Exercise 01

---
+++ Exercise 01/UseCard.jsx
@@ -0,0 +1,9 @@
+const  UseCard = ()=>{
+    return(
+        <div>
+            <h2>UserName: Nur abdi</h2>
+            <p>Email: Nurabdi1683@gmail.com</p>
+        </div>
+    )
+}
+export default UseCard;
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import UseCard from "../Exercise 01/UseCard";
+
+
+function app(){
+  return(
+    <>
+      <UseCard/>
+    </>
+    
+  )
+}
+export default app;
