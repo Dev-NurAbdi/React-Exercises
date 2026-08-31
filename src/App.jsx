1f45190ff2cfba4286cba6a9434bea15770f0627
nurabdi2776 <nurabdi2776@gmail.com>
2026-08-31 10:38:10 -0700

    Exercise 16

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
+
 import React, { useState } from 'react';
-import LanguageContext from '../React js Exercises/Exercise 15/LanguageContext';
-import Greeting from '../React js Exercises/Exercise 15/Greeting';
+import CartContext from '../React js Exercises/Exercise 16/CartContext';
+import ProductItem from '../React js Exercises/Exercise 16/ProductItem';
+import CartSummary from '../React js Exercises/Exercise 16/CartSummary';
 
 function App() {
-  const [language, setLanguage] = useState('en');
+  const [cartItems, setCartItems] = useState([]);
 
-  const toggleLanguage = () => {
-    setLanguage((prevLang) => (prevLang === 'en' ? 'es' : 'en'));
+  const addToCart = (item) => {
+    setCartItems([...cartItems, item]);
   };
 
+  const removeFromCart = (itemId) => {
+    setCartItems(cartItems.filter((item) => item.id !== itemId));
+  };
+
+  const value = { cartItems, addToCart, removeFromCart };
+
   return (
-    <LanguageContext.Provider value={language}>
-      <button onClick={toggleLanguage}>
-        Switch to {language === 'en' ? 'Spanish' : 'English'}
-      </button>
-      <Greeting />
-    </LanguageContext.Provider>
+    <CartContext.Provider value={value}>
+      <ProductItem itemId={1} itemName="Widget" price={19.99} />
+      <ProductItem itemId={2} itemName="Gadget" price={29.99} />
+      <CartSummary />
+    </CartContext.Provider>
   );
 }
 
