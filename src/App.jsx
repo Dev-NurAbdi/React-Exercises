326fa573bc81518a76c33755d1583a05529cfd59
nurabdi2776 <nurabdi2776@gmail.com>
2026-08-31 10:18:39 -0700

    Exercise 15

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import MultiStepForm from "../React js Exercises/Exercise 14/MultiStepForm";
+import React, { useState } from 'react';
+import LanguageContext from '../React js Exercises/Exercise 15/LanguageContext';
+import Greeting from '../React js Exercises/Exercise 15/Greeting';
 
 function App() {
+  const [language, setLanguage] = useState('en');
+
+  const toggleLanguage = () => {
+    setLanguage((prevLang) => (prevLang === 'en' ? 'es' : 'en'));
+  };
+
   return (
-    <>
-      <MultiStepForm />
-    </>
+    <LanguageContext.Provider value={language}>
+      <button onClick={toggleLanguage}>
+        Switch to {language === 'en' ? 'Spanish' : 'English'}
+      </button>
+      <Greeting />
+    </LanguageContext.Provider>
   );
 }
 
