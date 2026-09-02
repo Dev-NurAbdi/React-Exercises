b1aa7a3b05ac8dfbdd2502f9ef55748e84d6f0f9
nurabdi2776 <nurabdi2776@gmail.com>
2026-09-02 09:55:59 -0700

    Exercise 17

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
+import React from "react";
+import ContactForm from "../React js Exercises/Exercise 17/ContactForm";
 
-import React, { useState } from 'react';
-import CartContext from '../React js Exercises/Exercise 16/CartContext';
-import ProductItem from '../React js Exercises/Exercise 16/ProductItem';
-import CartSummary from '../React js Exercises/Exercise 16/CartSummary';
-
-function App() {
-  const [cartItems, setCartItems] = useState([]);
-
-  const addToCart = (item) => {
-    setCartItems([...cartItems, item]);
-  };
-
-  const removeFromCart = (itemId) => {
-    setCartItems(cartItems.filter((item) => item.id !== itemId));
-  };
-
-  const value = { cartItems, addToCart, removeFromCart };
-
+const App = () => {
   return (
-    <CartContext.Provider value={value}>
-      <ProductItem itemId={1} itemName="Widget" price={19.99} />
-      <ProductItem itemId={2} itemName="Gadget" price={29.99} />
-      <CartSummary />
-    </CartContext.Provider>
+    <div>
+      <h1>Contact Form</h1>
+
+      <ContactForm />
+    </div>
   );
-}
+};
 
 export default App;
